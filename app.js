f118cb5330b419d61f64dcd6332c83802227d894
M T <93407995+TorresMariana@users.noreply.github.com>
2025-03-04 15:22:17 -0300

    version final juego secreto

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-// let numeroSecreto = generarNumeroSecreto();
-// console.log('número secreto: ', numeroSecreto);
-// let intentos = 1;
 
-//CAMBIAR A:
 let numeroSecreto = 0;
 let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
 
 
-
-// let titulo = document.querySelector('h1');
-// titulo.innerHTML = 'Juego del número secreo';
-
-// let parrafo = document.querySelector('p');
-// parrafo.innerHTML = 'Indica un número del 1 al 10';
-
-
 function asignarTextoElemento(elemento, texto){
-    // let titulo = document.querySelector('h1');
-    // titulo.innerHTML = 'Juego del número secreto';
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
-    //Como buena practicar colocar RETURN al final de cada función, 
-    //aunque no retoen ningun resultado.
     return;
 }
 
 function verificarIntento(){
-    // alert('Click desde el botón');
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
     // console.log('Número secreto: ', numeroSecreto);
     console.log('Número usuario:', numeroDeUsuario);
@@ -55,8 +38,6 @@ function verificarIntento(){
 }
 
 function limpiarCaja(){
-    // let valorCaja = document.querySelector('#valorUsuario');
-    // valorCaja.value = '';
     document.querySelector('#valorUsuario').value = '';
     return;
 }
@@ -95,10 +76,6 @@ function condicionesIniciales(){
 function reiniciarJuego(){
     //Limpiar caja
     limpiarCaja();
-
-    //Visualizar de mensaje de inicio
-    //Generar el número aleatorio
-    //Inicializar el número de intentos
     condicionesIniciales();
 
     //Deshabilitar el bóton de nuevo juego
@@ -109,8 +86,6 @@ function reiniciarJuego(){
 
 
 //HOISTING
-// asignarTextoElemento('h1','Juego del número secreto');
-// asignarTextoElemento('p', 'Indica un número del 1 al 10');
 condicionesIniciales();
 
 
